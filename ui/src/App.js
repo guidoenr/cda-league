b8d941c14f9960047416c15a90a0b4e6b0a07be5
guidoenr <guidoenr4@gmail.com>
2023-01-09 12:51:52 -0300

    fixed everything

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,57 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import PlayersTable from './PlayersTable';
-import PlayersCard from './PlayersCard';
-
-
+import PlayersTableRank from './modules/PlayersTable';
+import PlayersCardsList from './modules/PlayersCard';
 
 
 function App(){
     return (
         <div>
-            {/*<PlayersList />*/}
-            <PlayersRanked />
+            <PlayersTableRank />
+            <PlayersCardsList />
         </div>
     )
-}
-
-
-
-function PlayersList() {
-    const [players, setPlayers] = useState([]);
-
-    useEffect(() => {
-        // Fetch players data from API
-        fetch('http://localhost:8080/players/')
-            .then(response => response.json())
-            .then(data => setPlayers(data.players));
-    }, []);
-
-    return (
-        <div className="center">
-            <h2>Players</h2>
-            <PlayersTable players={players} />
-        </div>
-    );
-}
-
-
-
-function PlayersRanked() {
-    const [players, setPlayers] = useState([]);
-
-    useEffect(() => {
-        // Fetch players data from API
-        fetch('http://localhost:8080/players/rank')
-            .then(response => response.json())
-            .then(data => setPlayers(data.players));
-    }, []);
-
-    return (
-        <div className="center">
-            <h2>Players Cards</h2>
-            <PlayersCard players={players} />
-        </div>
-    );
 }
 
 
