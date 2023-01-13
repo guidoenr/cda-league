d0f457dd24bab79a0bfd0360ad9d49f041725c25
guidoenr <guidoenr4@gmail.com>
2023-01-13 12:06:32 -0300

    guido:kali | 13-Jan-12:06

--- ui/src/modules/header/Navbar.js
+++ ui/src/modules/header/Navbar.js
@@ -1,3 +1,5 @@
+
+
 import React from 'react';
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import './Navbar.css'
@@ -7,19 +9,18 @@ const MyNavbar = () => {
     const logo = require(`../assets/carmen-league.png`)
     return (
         <Navbar sticky="top" expand="lg" className="navbar navbar-color">
-                    <Navbar.Brand href="/">
-                        <img src={logo} alt="CDA League" className="navbar-logo" />
-                    </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav navbar-toggler" className="navbar-hamburger" />
-                <Navbar.Collapse id="esponsive-navbar-nav">
-                    <Nav className="me-auto nav-size">
-                        <Nav.Link className="nav-link" href="/">Home</Nav.Link>
-                        <Nav.Link className="nav-link" href="players">Jugadores</Nav.Link>
-                        <Nav.Link className="nav-link" href="rank">Tabla/Rank</Nav.Link>
-                        <Nav.Link className="nav-link" href="match">Partido</Nav.Link>
-                        <Nav.Link className="nav-link" href="paddle">Paddle</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
+            <Navbar.Brand href="/">
+                <img src={logo} alt="CDA League" className="navbar-logo" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav navbar-toggler" className="navbar-hamburger" />
+            <Navbar.Collapse id="esponsive-navbar-nav">
+                <Nav className="me-auto nav-size">
+                    <Nav.Link className="nav-link" href="players">Jugadores</Nav.Link>
+                    <Nav.Link className="nav-link" href="rank">Tabla/Rank</Nav.Link>
+                    <Nav.Link className="nav-link" href="match">Partido</Nav.Link>
+                    <Nav.Link className="nav-link" href="paddle">Paddle</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     );
 };
