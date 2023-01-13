471ff07c9decd36d0e757e95b92cc6fc4b41c4d2
guidoenr <guidoenr4@gmail.com>
2023-01-12 22:39:01 -0300

    guido:kali | 12-Jan-22:39

--- ui/src/modules/header/Navbar.js
+++ ui/src/modules/header/Navbar.js
@@ -6,22 +6,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const MyNavbar = () => {
     const logo = require(`../assets/carmen-league.png`)
     return (
-        <Navbar className="mynavbar fixed-top" expand="lg" bg="light" >
-        <Container>
-            <Navbar.Brand href="/">
-                <img src={logo} alt="CDA League" width="180" height="120" className="d-inline-block align-top nav" />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto nav-size">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="players">Jugadores</Nav.Link>
-                    <Nav.Link href="rank">Tabla/Rank</Nav.Link>
-                    <Nav.Link href="match">Partido</Nav.Link>
-                    <Nav.Link href="paddle">Paddle</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Container>
+        <Navbar className="mynavbar fixed-top" expand="lg" >
+                <div className="navbar-logo">
+                    <Navbar.Brand href="/">
+                        <img src={logo} alt="CDA League" width="180" height="120" className="d-inline-block align-top nav" />
+                    </Navbar.Brand>
+                </div>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav hamburger">
+                    <Nav className="me-auto nav-size">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="players">Jugadores</Nav.Link>
+                        <Nav.Link href="rank">Tabla/Rank</Nav.Link>
+                        <Nav.Link href="match">Partido</Nav.Link>
+                        <Nav.Link href="paddle">Paddle</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
         </Navbar>
     );
 };
