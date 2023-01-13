2c69d45075431921aa80554d7d8d7101deb35388
guidoenr <guidoenr4@gmail.com>
2023-01-13 14:36:33 -0300

    guido:kali | 13-Jan-14:36

--- ui/src/modules/navbar/Navbar.js
+++ ui/src/modules/navbar/Navbar.js
@@ -6,7 +6,7 @@ import './Navbar.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const MyNavbar = () => {
-    const logo = require(`../assets/carmen-league.png`)
+    const logo = require(`./ui/src/assets/carmen-league.png`)
     return (
         <Navbar sticky="top" expand="lg" className="navbar navbar-color">
             <Navbar.Brand href="/">
