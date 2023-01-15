717c11b4d2860c31f1f4de7b577c23da21c612de
guidoenr <guidoenr4@gmail.com>
2023-01-15 13:44:33 -0300

    guido:kali | 15-Jan-13:44

--- ui/src/modules/player/PlayerCard.js
+++ ui/src/modules/player/PlayerCard.js
@@ -2,22 +2,23 @@ import React from 'react';
 import './PlayerCard.css'
 import '../Util'
 import {getStars, getPhoto, getColor} from "../Util";
+import Container from 'react-bootstrap/Container';
 
 const PlayerCard = ({player}) => {
 
     return (
-        <div className="player-card ">
-            <img src={getPhoto(player.nickname)} alt={`${player.name}'s profile photo`} className="player-photo" />
-            <div className="player-info">
-                <h2 className="player-nickname">{player.nickname}</h2>
-                <div className="player-rank">
+        <Container className="player-card">
+            <img src={getPhoto(player.nickname)} alt={`${player.name}'s profile photo`} className="photo" />
+            <div className="info">
+                <h2 className="nickname">{player.nickname}</h2>
+                <div className="rank">
                     {getStars(player.rank)}
                 </div>
-                <div className="player-name">{player.name}</div>
-                <div className="player-elo">ELO: {player.elo}</div>
+                <div className="name">{player.name}</div>
+                <div className="elo">ELO: {player.elo}</div>
                 <div className="position-shape" style={{backgroundColor: getColor(player.position)}}> {player.position.toUpperCase().substring(0,3)}</div>
             </div>
-        </div>
+        </Container>
     );
 }
 
