c930b637a7dee85d3647969ad4c32556408d004f
guidoenr <guidoenr4@gmail.com>
2023-01-20 18:13:40 -0300

    guido:kali | 20-Jan-18:13

--- ui/src/modules/match/Match.js
+++ ui/src/modules/match/Match.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Container from "react-bootstrap/Container";
 import PlayerCard from "../player/PlayerCard";
 import Team from "./Team";
 import jsonPlayers from "../../resources/players.json";
@@ -9,10 +8,10 @@ import "animate.css";
 
 const Match = () => {
     const [allPlayers, setAllPlayers] = useState([]);
-    let [availablePlayers, setAvailablePlayers] = useState([]);
     const [showTeams, setShowTeams] = useState(false);
     const [Team1, setTeam1] = useState({});
     const [Team2, setTeam2] = useState({});
+    let availablePlayers = []
 
     // obtain the players
     useEffect(() => {
@@ -34,37 +33,37 @@ const Match = () => {
 
     // generate match with selected players
     const generateMatchWithPlayers = () => {
-        const data = { players: availablePlayers };
-        fetch("http://localhost:8080/generateMatch/", {
-            method: "POST",
-            body: JSON.stringify(data),
-            headers: { "Content-Type": "application/json" },
-        })
-            .then((res) => res.json())
-            .then((response) => {
-                setTeam1(response.team1);
-                setTeam2(response.team2);
-                setShowTeams(true);
+        if (availablePlayers.length === 0) {
+            alert("AVAILABLE PLAYERS === 0")
+        } else {
+            const data = { players: availablePlayers };
+            console.log(data)
+            fetch("http://localhost:8080/generateMatch/", {
+                method: "POST",
+                body: JSON.stringify(data),
+                headers: { "Content-Type": "application/json" },
             })
-            .catch((error) => console.error("Error:", error));
+                .then((res) => res.json())
+                .then((response) => {
+                    setTeam1(response.team1);
+                    setTeam2(response.team2);
+                    setShowTeams(true);
+                })
+                .catch((error) => console.error("Error:", error));
+        }
+
     };
 
     // click the playerCard
     const handleSelectPlayer = (player) => {
-        if (player.selected) {
-            availablePlayers.pop(player);
-            player.selected = false;
-        } else {
-            player.selected = true;
             availablePlayers.push(player);
-        }
     };
 
     const cdaLogo = require("../../assets/cda-league-only-logo.png");
     function renderTeams() {
         if (showTeams) {
             return (
-                <Container>
+                <div className="container">
                     <div className="match-container animate__animated animate__fadeInDown ">
                         <div className="match">
                             <div className="match-header">
@@ -95,33 +94,29 @@ const Match = () => {
                             </div>
                         </div>
                     </div>
-                </Container>
+                </div>
             );
-        } else {
-            return <Container></Container>;
         }
     }
 
     return (
-        <Container>
+        <div className="container">
             <div className="note-container">
                 <p className="match-note">Seleccionar jugadores</p>
             </div>
             <div className="available-players-container">
                 {allPlayers.map((player) => (
-                    <PlayerCard key={player.id} player={player} handleSelectPlayer={handleSelectPlayer} />
+                    <PlayerCard key={player.id} player={player} onClick={handleSelectPlayer} />
                 ))}
             </div>
-            <Container>
                 <div className="button-container">
                     <Button
                         onClick={generateMatchWithPlayers}
                         textToDisplay="Armar partido"
                     />
                 </div>
-            </Container>
             {renderTeams()}
-        </Container>
+        </div>
     );
 };
 export default Match;
